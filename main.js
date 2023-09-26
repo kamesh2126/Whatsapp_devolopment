28dffbec547c695647b1d2ae560839d2f42e6bed
kamesh2126 <124708966+kamesh2126@users.noreply.github.com>
2023-09-26 19:58:32 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,46 @@
+//strings
+const myvariable="423.098";
+const myNumber = 42;
+const myfloat = 42.09;
+//string methods
+console.log(myvariable.split(""));
+// Numbers methods
+console.log(Number.isInteger(42));
+console.log(Number.parseFloat(myvariable));
+console.log(Number.parseInt(myvariable));
+console.log(Number(parseFloat(myvariable)));
+console.log(Number(parseFloat(myvariable)).toFixed);
+console.log(myfloat.toString());
+console.log(myNumber.toString());
+console.log(typeof myNumber.toString());
+console.log(typeof myfloat.toString());
+console.log(Number.nu)
+//math properties
+console.log(Math.PI)
+console.log(Math.trunc(Math.PI));
+console.log(Math.round(4.2));
+console.log(Math.ceil(4.1));
+console.log(Math.floor(4.1)); 
+console.log(Math.pow(2,8));
+console.log(Math.min(10,3));
+console.log(Math.max(10,9,8,7));
+console.log(Math.random()*10);
+console.log(Math.floor(Math.random()));
+console.log(Math.ceil(Math.random()*10));
+  //Math method and propeerties
+  // 1 to 100
+const myVariable = "kameshbabu";
+console.log(typeof myVariable);
+console.log(myVariable.charAt(0));
+console.log(myVariable.charAt(Math.floor(Math.random()*9)));
+console.log(myVariable.charAt(Math.ceil(Math.random()*9)-1));
+const myName = "arunachalam";
+console.log(myName.charAt(Math.floor(Math.random()*myName.length)));
+console.log(myVariable.charAt(Math.floor(Math.random()*myVariable.length)));
+console.log(myName.charAt(Math.ceil(Math.random()*myName.length)));
+console.log(myVariable.charAt(Math.ceil(Math.random()*myVariable.length)));
+const mynumber= "1234567890"
+console.log(mynumber.charAt(Math.floor(Math.random()*mynumber.length)));
+//if statements
+    
+                                                                                                                                          
